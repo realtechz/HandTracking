96b5c1f5978d116b17938009293915cc1ec2494e
realtechz <64344653+realtechz@users.noreply.github.com>
2021-07-06 22:41:46 +0530

    v0.1

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import time
 
 
-class handDetector():
+class handDetector:
 
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
